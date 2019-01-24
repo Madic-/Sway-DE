89546378c49ec0b7224c9f644c7ca0e9b0c1bd58
Madic- <3735459+Madic-@users.noreply.github.com>
2019-01-24 16:10:29 +0100

    - Make i3bar transparent - Add i3status.conf

--- setup.sh
+++ setup.sh
@@ -23,37 +23,38 @@ if [ ! -d "$ROFI_DIR" ]; then mkdir -p "$ROFI_DIR"; fi
 echo -e "\nCloning and installing csxr's i3-gnome repository..."
 cd "$GIT_REPO_DIR" || exit
 if [ -z "$(ls -A $GIT_REPO_DIR)" ]; then
-  git clone https://github.com/csxr/i3-gnome .
+	git clone https://github.com/csxr/i3-gnome .
 else
-  git pull https://github.com/csxr/i3-gnome
+	git pull https://github.com/csxr/i3-gnome
 fi
 sudo make install
 
 echo -e "\nCopying config files from $SCRIPT_DIR/config..."
 cd "$SCRIPT_DIR"
 cp "$SCRIPT_DIR/config/i3_config" "$I3_DIR/config"
+cp "$SCRIPT_DIR/config/i3status.conf" "$I3_DIR/i3status.conf"
 cp "$SCRIPT_DIR/config/compton_config" "$COMPTON_DIR/config"
 cp "$SCRIPT_DIR/config/rofi_config" "$ROFI_DIR/config"
 cp "$SCRIPT_DIR/config/Xresources" "$HOME/.Xresources"
 cp "$SCRIPT_DIR/config/Xresources.molokai" "$HOME/.Xresources.molokai"
 
 if ! grep -q "/.Xresources.molokai" "$HOME/.Xresources"; then
-  echo "#include \"$HOME/.Xresources.molokai\"" >> "$HOME/.Xresources"
+	echo "#include \"$HOME/.Xresources.molokai\"" >>"$HOME/.Xresources"
 fi
 
 if ! grep -q "i3-gnome-flashback" "$HOME/.bashrc"; then
-  echo -e "\nConfiguring $HOME/.bashrc..."
-  cat <<EOF | sudo tee -a "$HOME/.bashrc" > /dev/null
+	echo -e "\nConfiguring $HOME/.bashrc..."
+	cat <<EOF | sudo tee -a "$HOME/.bashrc" >/dev/null
 
 # i3-gnome-flashback config begin
 PS1="\[\033[1;32m\][\u@\h:\w]\\$ \[$(tput sgr0)\]"
 export PAGER=most
 export EDITOR=nano
 
-alias nano="nano -c"
-alias en="sudo -i"
-alias ll="ls -lah --group-directories-first"
-alias mkdir="mkdir -pv"
+alias nano='nano -c'
+alias en='sudo -i'
+alias ll='ls -lah --group-directories-first'
+alias mkdir='mkdir -pv'
 alias wget='wget -c'
 alias df='df -H'
 alias du='du -ch'
@@ -62,18 +63,18 @@ EOF
 fi
 
 if ! sudo grep -q "i3-gnome-flashback" "/root/.bashrc"; then
-  echo -e "\nConfiguring /root/.bashrc..."
-  cat <<EOF | sudo tee -a /root/.bashrc > /dev/null
+	echo -e "\nConfiguring /root/.bashrc..."
+	cat <<EOF | sudo tee -a /root/.bashrc >/dev/null
 
 # i3-gnome-flashback config begin
 PS1="\[\033[1;31m\][\u@\h:\w]\\$ \[$(tput sgr0)\]"
 export PAGER=most
 export EDITOR=nano
 
-alias nano="nano -c"
-alias en="sudo -i"
-alias ll="ls -lah --group-directories-first"
-alias mkdir="mkdir -pv"
+alias nano='nano -c'
+alias en='sudo -i'
+alias ll='ls -lah --group-directories-first'
+alias mkdir='mkdir -pv'
 alias wget='wget -c'
 alias df='df -H'
 alias du='du -ch'
