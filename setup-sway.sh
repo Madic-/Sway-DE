c4b773b6d1de4bdd47b5e4c7128c690baec4a9f6
Madic- <3735459+Madic-@users.noreply.github.com>
2019-02-28 00:38:07 +0100

    Added ssh-agent on login

--- setup-sway.sh
+++ setup-sway.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SWAY_DIR="$HOME/.config/sway"
 ROFI_DIR="$HOME/.config/rofi"
 BLOCKS_DIR="$HOME/bin/blocks"
+SYSTEMD_UDIR="$HOME/.config/systemd/user"
 
 if [ -f /etc/os-release ]; then . /etc/os-release; OS=$NAME; fi
 
@@ -22,6 +23,7 @@ if [ ! -d "$SWAY_DIR" ]; then mkdir -p "$SWAY_DIR"; fi
 if [ ! -d "$ROFI_DIR" ]; then mkdir -p "$ROFI_DIR"; fi
 if [ ! -d "$HOME/bin" ]; then mkdir -p "$HOME/bin"; fi
 if [ ! -d "$BLOCKS_DIR" ]; then mkdir -p "$BLOCKS_DIR"; fi
+if [ ! -d "$SYSTEMD_UDIR" ]; then mkdir -p "$SYSTEMD_UDIR"; fi
 
 echo -e "\nCopying config files from $SCRIPT_DIR/config..."
 cd "$SCRIPT_DIR" || exit
@@ -31,6 +33,12 @@ ln -s "$SCRIPT_DIR/config/rofi.conf" "$ROFI_DIR/config"
 ln -s "$SCRIPT_DIR/bin/blocks/wlan" "$BLOCKS_DIR/wlan"
 ln -s "$SCRIPT_DIR/bin/lock-sway" "$HOME/bin/lock-sway"
 ln -s "$SCRIPT_DIR/bin/s" "$HOME/bin/s"
+cp "$SCRIPT_DIR/config/ssh-agent.service" "$SYSTEMD_UDIR/"
+
+echo -e "\nEnabling ssh-agent..."
+sed s#HOME_VARIABLE#$HOME# "$SYSTEMD_UDIR/ssh-agent.service"
+systemctl --user daemon-reload
+systemctl --user enable ssh-agent.service
 
 echo -e "\nDownloading i3blocks scripts..."
 wget -q https://raw.githubusercontent.com/vivien/i3blocks-contrib/master/bandwidth3/bandwidth3 -O "$BLOCKS_DIR/bandwidth3"
