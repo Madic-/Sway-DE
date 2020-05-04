84b8e0e840622dba6c09dfb9fb6351d1ad475cc0
Madic- <3735459+Madic-@users.noreply.github.com>
2020-05-04 18:59:01 +0200

    Added screensharing. Fixes #4

---
+++ bin/screenshare.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+geometry() {
+  windowGeometries=$(
+    # `height - 1` is there because of: https://github.com/ammen99/wf-recorder/pull/56 (I could remove it if it's merged, maybe)
+    swaymsg -t get_workspaces -r | jq -r '.[] | select(.focused) | .rect | "\(.x),\(.y) \(.width)x\(.height - 1)"'
+    swaymsg -t get_outputs -r | jq -r '.[] | select(.active) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'
+  )
+  geometry=$(slurp -b "#45858820" -c "#45858880" -w 3 -d <<<"$windowGeometries") || exit $?
+  echo "$geometry"
+}
+
+{
+  if [ "$1" == "stop" ]; then
+    if pgrep ffplay >/dev/null; then
+      pkill ffplay >/dev/null
+    fi
+    if pgrep wf-recorder >/dev/null; then
+      pkill wf-recorder >/dev/null
+    fi
+    notify-send -t 2000 "Wayland recording has been stopped"
+  elif [ "$1" == "is-recording" ]; then
+    if pgrep wf-recorder >/dev/null; then
+      notify-send -t 2000 "Wayland recording is up"
+    else
+      notify-send -t 2000 "No Wayland recording"
+    fi
+  else
+    if ! pgrep wf-recorder >/dev/null; then
+      geometry=$(geometry) || exit $?
+      unset SDL_VIDEODRIVER
+      wf-recorder -c rawvideo -x yuv420p -m avi -f >(ffplay -window_title Screenshare -f avi -) --geometry="$geometry" &
+      sleep 0.5
+      # a hack so FPS is not dropping
+      swaymsg [title=Screenshare] floating enable, move position 1800 900
+    fi
+    notify-send -t 2000 "Wayland recording has been started"
+  fi
+} >~/.wayland-share-screen.log 2>&1
---
+++ screenshots/screensharing.md
@@ -0,0 +1,19 @@
+# Screensharing
+
+Screensharing is based upon a solution in a [reddit thread](https://www.reddit.com/r/swaywm/comments/di7ev1/screensharing_w_sway_possible/) and modified to not require the v4l2loopback kernel module and piping the content from wf-recorder directly to ffplay.
+
+Basically it records the desktop, or a selectable part of it, with wf-recorder and opens the recorded part with ffplay in an X-window. The X-window can than be selected as an application in every screenshare scenario (e.g. Zoom or Jitsi).
+
+## Usage
+
+Start screensharing with $mod+x
+
+![Screenshare01](screenshare01.png)
+
+* s = start screensharing
+* t = stop screensharing
+* a = is screensharing running
+
+When starting a new scrensharing session, you can select to either record the complete screen or only an application. Please keep in mind that in the background this will only generate a region for wf-recorder to record. Every application that is visible inside this region will be recorded. After selecting the region, an ffplay floating window with the title "Screenshare" should open in the bottom left corner. This is the window you select as screensharing target.
+
+![Screenshare02](screenshare02.png)
