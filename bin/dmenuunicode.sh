706301f04cf9e71204f2f6d416f75768dce647fe
Madic- <3735459+Madic-@users.noreply.github.com>
2020-06-26 00:00:58 +0200

    - Added bemenu-run shell script - Fixed dmenuunicode for wayland - added hotkey for dmenuunicode

--- bin/dmenuunicode.sh
+++ bin/dmenuunicode.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
+# Original from Luke Smith:
+# https://github.com/LukeSmithxyz/voidrice/blob/master/.local/bin/dmenuunicode
 
 # The famous "get a menu of emojis to copy" script.
 
 # Get user selection via dmenu from emoji file.
-chosen=$(cut -d ';' -f1 ~/.local/bin//emoji | dmenu -n -i -l 30 -p '' --tb "#1a1a1a" --tf "#268bd2" --fb "#2E3440" --nb "#1a1a1a" --hb "#1a1a1a" --hf "#268bd2" -m $(swaymsg -r -t get_outputs | jq '. | reverse | to_entries | .[] | select(.value.focused == true) | .key') | sed "s/ .*//")
+#chosen=$(cut -d ';' -f1 ~/.local/bin//emoji | dmenu -n -i -l 30 -p '' --tb "#1a1a1a" --tf "#268bd2" --fb "#2E3440" --nb "#1a1a1a" --hb "#1a1a1a" --hf "#268bd2" -m $(swaymsg -r -t get_outputs | jq '. | reverse | to_entries | .[] | select(.value.focused == true) | .key') | sed "s/ .*//")
+chosen=$(cut -d ';' -f1 ~/.local/bin/emoji | ~/.local/bin/bemenu-run.sh list | sed "s/ .*//")
 
 # Exit if none chosen.
 [ -z "$chosen" ] && exit
 
-echo "$chosen" | tr -d '\n' | wl-copy && notify-send "'$chosen' copied to clipboard." &
-
 # If you run this command with an argument, it will automatically insert the
 # character. Otherwise, show a message that the emoji has been copied.
-#if [ -n "$1" ]; then
-#	xdotool type "$chosen"
-#else
-#	echo "$chosen" | tr -d '\n' | wl-copy
-#	notify-send "'$chosen' copied to clipboard." &
-#fi
+#
+# ydotool works only passwordless if the user has permissions to write to /dev/uinput
+# https://github.com/ReimuNotMoe/ydotool/issues/25
+
+if [ -x "$(command -v ydotool)" ] && [ -n "$1" ]; then
+  echo "$chosen" | tr -d '\n' | wl-copy
+  notify-send "'$chosen' copied to clipboard and entered." &
+
+  if [ "$(swaymsg -r -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused==true) | .app_id')" = 'Alacritty' ]; then
+    ydotool key ctrl+shift+v
+  else
+    ydotool key ctrl+v
+  fi
+else
+  echo "$chosen" | tr -d '\n' | wl-copy
+  notify-send "'$chosen' copied to clipboard." &
+fi
