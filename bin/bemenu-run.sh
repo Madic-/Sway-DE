ad39f0b34f614e390ad74c8cfa0eb75e035a9aff
Madic- <3735459+Madic-@users.noreply.github.com>
2020-06-29 14:23:35 +0200

    Re-added bemenu-run mode to bemenu-run

--- bin/bemenu-run.sh
+++ bin/bemenu-run.sh
@@ -2,4 +2,8 @@
 
 BEMENU_ARGS=(-n -i -p '' --tb "#1a1a1a" --tf "#268bd2" --fb "#2E3440" --nb "#1a1a1a" --hb "#1a1a1a" --hf "#268bd2" -m $(swaymsg -r -t get_outputs | jq '. | reverse | to_entries | .[] | select(.value.focused == true) | .key') "$@")
 
-bemenu "${BEMENU_ARGS[@]}"
+if [ "$1" = 'dmenu' ]; then
+  bemenu-run "${BEMENU_ARGS[@]}"
+else
+  bemenu "${BEMENU_ARGS[@]}"
+fi
