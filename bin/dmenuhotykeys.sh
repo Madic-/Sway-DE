4b9de91555508e4148c8cd69f1ca275ffb3e0fed
Madic- <3735459+Madic-@users.noreply.github.com>
2020-07-01 07:58:35 +0200

    - Open text files with $EDITOR in lf - Added dmenuhotkeys script

---
+++ bin/dmenuhotykeys.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+swaymsg "$(sed -E 's/^bindsym\s*(\S*)\s*(\S.*$)/\1\\\2/;t;d' "$HOME/.config/sway/sway.d/07_hotkeys.conf" | column -s'\' -t | bemenu-run.sh -l 30 | cut -d' ' -f2- | xargs -0)"
