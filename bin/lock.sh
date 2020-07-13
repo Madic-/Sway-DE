5a20a72eb8ca14eb87c2c576e40dc768dd55861c
Madic- <3735459+Madic-@users.noreply.github.com>
2020-07-13 23:20:37 +0200

    Show failed unlock attempts

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,4 +2,4 @@
 
 ## Lockscreen script
 
-swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 12 --effect-blur 7x5 --ring-color 2E3440 --key-hl-color ECEFF4 --line-color 88C0D0 --inside-color 00000088 --separator-color 00000000 --datestr %Y-%m-%d --text-color ECEFF4 --text-caps-lock-color ECEFF4
+swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 12 --effect-blur 7x5 --ring-color 2E3440 --key-hl-color ECEFF4 --line-color 88C0D0 --inside-color 00000088 --separator-color 00000000 --datestr %Y-%m-%d --text-color ECEFF4 --text-caps-lock-color ECEFF4 --show-failed-attempts
