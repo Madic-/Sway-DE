fcfc0a3dcf9d636ba35553248ec321b7a2944fdc
Madic- <3735459+Madic-@users.noreply.github.com>
2020-09-11 15:22:07 +0200

    Added "Suspend then hibernate" option

--- bin/dmenupower.sh
+++ bin/dmenupower.sh
@@ -2,9 +2,11 @@
 
 ## Shows a drop down menu with power options
 
-case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot" | "$HOME"/.local/bin/bemenu-run.sh -l 4 -p "Power:")" in
+case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n⏲ Suspend then hibernate\n" | "$HOME"/.local/bin/bemenu-run.sh -l 5 -p "Power:")" in
 " Exit sway") swaymsg exit ;;
 " Lock") ~/.local/bin/lock.sh ;;
 " Power Off") systemctl poweroff ;;
 " Reboot") systemctl reboot ;;
+"⏲ Suspend then hibernate") systemctl suspend-then-hibernate;;
 esac
+
