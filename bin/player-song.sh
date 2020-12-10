099b320a6fc5c09a1591372bce5b0ed834bfc3a2
Madic- <3735459+Madic-@users.noreply.github.com>
2020-12-10 14:21:43 +0100

    playerctl: Ignore firefox and delete icon after usage

--- bin/player-song.sh
+++ bin/player-song.sh
@@ -3,11 +3,12 @@
 set -e
 
 ICON=$HOME/.local/share/icons/target
-THUMB=$(playerctl metadata --format '{{lc(mpris:artUrl)}}')
-SONG=$(playerctl metadata --format "{{ title }}\n{{ artist }} - {{ album }}")
+THUMB=$(playerctl -i firefox metadata --format '{{lc(mpris:artUrl)}}')
+SONG=$(playerctl -i firefox metadata --format "{{ title }}\n{{ artist }} - {{ album }}")
 
 if [ -n "$THUMB" ]; then
   convert "$THUMB" -flatten -thumbnail 256x256 "$ICON"
 fi
 
 notify-send -i "$ICON" "Now playing" "$SONG"
+if [ -f "$ICON" ]; then rm "$ICON"; fi
