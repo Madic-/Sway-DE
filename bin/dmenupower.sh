e75e7e604479b9db3b8dbc0443ffccce810c119d
Madic- <3735459+Madic-@users.noreply.github.com>
2021-03-29 17:16:49 +0200

    Also shutdown when idle_inhibitor present

--- bin/dmenupower.sh
+++ bin/dmenupower.sh
@@ -5,8 +5,7 @@
 case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n⏲ Suspend then hibernate\n" | "$HOME"/.local/bin/bemenu-run.sh -l 5 -p "Power:")" in
 " Exit sway") swaymsg exit ;;
 " Lock") ~/.local/bin/lock.sh ;;
-" Power Off") systemctl poweroff ;;
-" Reboot") systemctl reboot ;;
-"⏲ Suspend then hibernate") systemctl suspend-then-hibernate;;
+" Power Off") exec systemctl poweroff -i ;;
+" Reboot") exec systemctl reboot ;;
+"⏲ Suspend then hibernate") exec systemctl suspend-then-hibernate ;;
 esac
-
