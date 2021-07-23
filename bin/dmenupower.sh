214b08ee1693e8f42a7c4105012a73c94103c796
Madic- <3735459+Madic-@users.noreply.github.com>
2021-07-23 23:38:27 +0200

    Fixed path

--- bin/dmenupower.sh
+++ bin/dmenupower.sh
@@ -3,8 +3,11 @@
 ## Shows a drop down menu with power options
 
 case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n⏲ Suspend then hibernate\n" | bemenu-run.sh -l 5 -p "Power:")" in
-" Exit sway") swaymsg exit; loginctl terminate-user $USER ;;
-" Lock") ~/.local/bin/lock.sh ;;
+" Exit sway")
+  swaymsg exit
+  loginctl terminate-user $USER
+  ;;
+" Lock") lock.sh ;;
 " Power Off") exec systemctl poweroff -i ;;
 " Reboot") exec systemctl reboot ;;
 "⏲ Suspend then hibernate") exec systemctl suspend-then-hibernate ;;
