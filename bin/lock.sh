7e0789c90b3986580ab9ccf97b7c8e65ec7c457e
Madic- <3735459+Madic-@users.noreply.github.com>
2022-06-26 12:21:03 +0200

    Dynamic lockscreen wallpapers from bing

--- bin/lock.sh
+++ bin/lock.sh
@@ -5,20 +5,43 @@
 ## No modifications were made on the lockicon
 ## Lockicon License: https://creativecommons.org/licenses/by/4.0/
 
-swaylock --daemonize --screenshots --clock --indicator \
-  --indicator-radius 100 \
+#swaylock --daemonize --screenshots --clock --indicator \
+#  --indicator-radius 100 \
+#  --indicator-thickness 12 \
+#  --effect-blur 7x5 \
+#  --ring-color 2E3440 \
+#  --key-hl-color ECEFF4 \
+#  --line-color 88C0D0 \
+#  --inside-color 00000088 \
+#  --separator-color 00000000 \
+#  --datestr %Y-%m-%d \
+#  --text-color ECEFF4 \
+#  --text-caps-lock-color ECEFF4 \
+#  --show-failed-attempts \
+#  --fade-in 0.1 \
+#  --effect-scale 0.5 --effect-blur 8x3 --effect-scale 2 \
+#  --effect-vignette 0.5:0.5 \
+#  --effect-compose "1.5%,1.5%;-1x10%;$HOME/.config/sway/sway.d/lock.png"
+
+bash bingwallpaper &>/dev/null &
+RANDOMPIC=$(ls ~/Pictures/Bing/*.jpg* | sort -R | tail -1)
+
+swaylock --daemonize --clock --indicator \
+  --image "$RANDOMPIC" \
+  --scaling stretch \
+  --indicator-radius 150 \
   --indicator-thickness 12 \
-  --effect-blur 7x5 \
   --ring-color 2E3440 \
   --key-hl-color ECEFF4 \
   --line-color 88C0D0 \
-  --inside-color 00000088 \
+  --inside-color 00000040 \
   --separator-color 00000000 \
   --datestr %Y-%m-%d \
   --text-color ECEFF4 \
   --text-caps-lock-color ECEFF4 \
   --show-failed-attempts \
-  --fade-in 0.1 \
-  --effect-scale 0.5 --effect-blur 8x3 --effect-scale 2 \
+  --fade-in 0.2 \
+  --effect-scale 0.5 --effect-scale 2 \
   --effect-vignette 0.5:0.5 \
-  --effect-compose "1.5%,1.5%;-1x10%;$HOME/.config/sway/sway.d/lock.png"
+  --effect-compose "1.5%,1.5%;-1x10%;$HOME/.config/sway/sway.d/lock.png" \
+  --effect-compose "42%,36%;-1x28%;$HOME/Pictures/kBz4LzR2_400x400_cropped.png"
