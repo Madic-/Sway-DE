76c217189613f55ffa2ce966cb018e750b2445e0
Madic- <3735459+Madic-@users.noreply.github.com>
2022-06-26 12:03:46 +0200

    Kanshi restart in power menu

--- bin/dmenupower.sh
+++ bin/dmenupower.sh
@@ -2,7 +2,7 @@
 
 ## Shows a drop down menu with power options
 
-case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n⏲ Suspend then hibernate\n" | bemenu-run.sh -l 5 -p "Power:")" in
+case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n Restart kanshi\n⏲ Suspend then hibernate\n" | bemenu-run.sh -l 6 -p "Power:")" in
 " Exit sway")
   swaymsg exit
   loginctl terminate-user $USER
@@ -10,5 +10,6 @@ case "$(echo -e " Exit sway\n Lock\n Power Off\n Reboot\n⏲ Suspend
 " Lock") lock.sh ;;
 " Power Off") exec systemctl poweroff -i ;;
 " Reboot") exec systemctl reboot ;;
+" Restart kanshi") exec systemctl --user restart kanshi ;;
 "⏲ Suspend then hibernate") exec systemctl suspend-then-hibernate ;;
 esac
