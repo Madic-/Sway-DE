6c9aefe1531b8077255194f435a45b1e15bc606a
Madic- <3735459+Madic-@users.noreply.github.com>
2022-07-07 19:44:21 +0200

    Test if timetagger is installed before running

--- config/waybar/scripts/timetagger-active.sh
+++ config/waybar/scripts/timetagger-active.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-TIMETAGGERB=timetagger
+if ! command -v timetagger &> /dev/null; then
+  exit 1
+fi
 
 if [[ "$1" == app ]]; then
-  "$TIMETAGGERB" app
+  timetagger app
   exit
 fi
 
-if [[ "$($TIMETAGGERB status | sed -n -e "4p")" == "Running: N/A"* ]]; then
+if [[ "$(timetagger status | sed -n -e "4p")" == "Running: N/A"* ]]; then
   echo "No time tracking"
 else
-  TIME=$($TIMETAGGERB status | sed -n -e "4p" | awk -F ' ' '{ print $2}')
+  TIME=$(timetagger status | sed -n -e "4p" | awk -F ' ' '{ print $2}')
   echo " $TIME"
 fi
